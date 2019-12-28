950e6fd388ff071c3acd447431cbdcd5cbed17ec
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-28 09:47:14 -0600

    Se agrego la funcionalidad de watch y Coverage. Se termino la clase: 'Watch y Coverage.'

---
+++ src/__test__/index.test.js
@@ -0,0 +1,15 @@
+const randomStrings = require("../index");
+
+// test("Probar la funcionalidad", () => {
+//   expect(typeof randomStrings()).toBe("string");
+// });
+
+describe("Probar funcionalidades de randomStrings", () => {
+  test("Probar la funcionalidad", () => {
+    expect(typeof randomStrings()).toBe("string");
+  });
+
+  test("Comprobar que no existe una ciudad", () => {
+    expect(randomStrings()).not.toMatch(/Cordoba/);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const cities = [
+  "Ciudad de Mexico",
+  "Botoga",
+  "Lima",
+  "Buenos Aires",
+  "Guadalajara"
+];
+
+const randomString = () => {
+  const string = cities[Math.floor(Math.random() * cities.length)];
+  return string;
+};
+
+const reverseString2 = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error("Error"));
+    }
+    resolve(
+      str
+        .split("")
+        .reverse()
+        .join("")
+    );
+  });
+};
+
+module.exports = randomString;
