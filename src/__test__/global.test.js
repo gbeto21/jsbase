a7896a32114924e36302df3d187339476028c925
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-28 09:05:04 -0600

    Se agregaron pruebas para boleanos y para numeros. Se termino la clase: 'Implementando pruebas para Boolean y Array.'

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,33 @@
 const text = "Hola Mundo";
+const fruits = ["manzana", "melon", "banana"];
 
 test("Debe contener un texto", () => {
   expect(text).toMatch(/Mundo/);
+});
+
+test("Tenemos una banana?", () => {
+  expect(fruits).toContain("banana");
+});
+
+test("Mayor que otro", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test("Vedadero", () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(
+    str
+      .split("")
+      .reverse()
+      .join("")
+  );
+};
+
+test("Probar un Callback", () => {
+  reverseString("Hola", str => {
+    expect(str).toBe("aloH");
+  });
 });
